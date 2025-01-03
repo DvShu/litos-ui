bf90b4c77212a2a056e41580688e6e88fc274a76
DvShu <tenny.shu@foxmail.com>
2025-01-03 17:35:40 +0800

    feat: Popover

--- src/components/popover/index.ts
+++ src/components/popover/index.ts
@@ -1,6 +1,11 @@
 import BaseComponent from "../base";
-import { initAttr } from "../utils";
-import { $$, $one, on, off } from "ph-utils/dom";
+import {
+  initAttr,
+  getPopoverOffsetX,
+  getPopoverOffsetY,
+  impactDetect,
+} from "../utils";
+import { $$, $one, on, off, $ } from "ph-utils/dom";
 
 type PlacementProp =
   | "top-start"
@@ -28,6 +33,14 @@ export default class Popover extends BaseComponent {
   public trigger: TriggerProp = "hover";
   public disabled = false;
   public isShow = false;
+  /** 宽度, 'trigger' 表示 popover 的宽度会和它的触发元素一致 */
+  public width?: string | number;
+  /** 浮层偏移量 */
+  public offset = 10;
+  /** 浮层定位方式 */
+  public position: "absolute" | "fixed" = "absolute";
+
+  private _t?: number;
   constructor() {
     super();
     initAttr(this);
@@ -61,6 +74,7 @@ export default class Popover extends BaseComponent {
         `l-popover-${this.placement}`,
         this.contentClass,
       ],
+      style: `--l-popover-offset: ${this.offset};`,
     });
     $content.innerHTML = this.content ? this.content : "<slot></slot>";
     if (this.showArrow) {
@@ -70,6 +84,13 @@ export default class Popover extends BaseComponent {
       $content.appendChild($arrow);
     }
     this.root.appendChild($content);
+  }
+
+  hideContent() {
+    const $content = $one(".l-popover--content", this.root);
+    if ($content) {
+      this.root.removeChild($content);
+    }
   }
 
   #initEvents() {
@@ -92,9 +113,103 @@ export default class Popover extends BaseComponent {
     this.#showFn(e.target as HTMLElement);
   };
 
-  #hanldeMouseLeave = () => {};
+  #hanldeMouseLeave = () => {
+    this.#hideFn();
+  };
 
   #handleClick = () => {};
 
-  #showFn($referece: HTMLElement) {}
+  #showFn($referece: HTMLElement) {
+    if (this.disabled) return;
+    if (this.isShow) {
+      this.#clearHide();
+      return;
+    }
+    this.isShow = true;
+    this.renderContent();
+    this.#updatePosition($referece);
+  }
+
+  #hideFn() {
+    this._t = setTimeout(() => {
+      this.isShow = false;
+      this.hideContent();
+    }, 50) as unknown as any;
+  }
+
+  #clearHide() {
+    if (this._t) {
+      clearTimeout(this._t);
+      this._t = undefined;
+    }
+  }
+
+  #updatePosition($referece: HTMLElement) {
+    // 获取水平和垂直方向的位置
+    let mainPos = "bottom";
+    let crossPos = "";
+    const poss = this.placement.split("-");
+    if (poss != null) {
+      mainPos = poss[0];
+      crossPos = poss[1] || "";
+    }
+    let width: number | undefined = undefined;
+    const $content = $one(".l-popover--content", this.root);
+    if ($content) {
+      let popoverRect = $content.getBoundingClientRect();
+      const targetRect = $referece.getBoundingClientRect();
+
+      // 获取滚动容器
+      let container = document.documentElement;
+      // 滚动条水平方向滚动距离
+      const scrollLeft = container.scrollLeft;
+      // 滚动条垂直方向滚动距离
+      const scrollTop = container.scrollTop;
+
+      if (this.width != null) {
+        if (this.width === "trigger") {
+          width = targetRect.width;
+        } else {
+          width = this.width as number;
+        }
+      }
+      if (width != null) {
+        popoverRect.width = width;
+      }
+      // 获取水平、垂直方向弹窗坐标点偏移
+      const yOffset = getPopoverOffsetY(
+        targetRect,
+        popoverRect,
+        mainPos,
+        crossPos,
+        this.offset
+      );
+      const xOffset = getPopoverOffsetX(
+        targetRect,
+        popoverRect,
+        mainPos,
+        crossPos,
+        this.offset
+      );
+      // 碰撞检测
+      const impactRes = impactDetect(
+        targetRect,
+        popoverRect,
+        mainPos,
+        crossPos,
+        scrollLeft,
+        scrollTop,
+        xOffset,
+        yOffset,
+        this.offset
+      );
+      mainPos = impactRes.mainAlign;
+      crossPos = impactRes.crossAlign;
+
+      if (width != null) {
+        $content.style.setProperty("width", `${width}px`);
+        const placement = `${mainPos}${crossPos === "" ? "" : "-" + crossPos}`;
+      }
+    }
+  }
 }
