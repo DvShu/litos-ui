29cf50a1be46c419fd7a4d449b0e06d08bc523de
Tenny <joel.shu@qq.com>
2025-01-10 00:53:18 +0800

    fix: 修复tree shaking

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 class Button {
   public static baseName = "button";
   #abc() {}
@@ -5,3 +6,9 @@ class Button {
 
 const a = new Button();
 console.log(Button.baseName);
+=======
+import { regist } from "./components/index";
+import Button from "./components/button";
+
+regist([Button]);
+>>>>>>> 4c4a6c8 (fix: 修复tree shaking)
