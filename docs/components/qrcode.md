1a06f682fc85c9b6a8f9917086d1838157920177
DvShu <tenny.shu@foxmail.com>
2025-06-26 18:02:17 +0800

    feat(qrcode): 实现二维码组件功能并更新文档
    - 添加二维码生成功能，使用 qrcode-generator-es 库进行渲染
    - 支持自定义文本、颜色、大小和中心图标
    - 更新组件文档，添加使用示例和参数说明
    - 添加相关示例图片资源
    - 更新项目依赖版本

--- docs/components/qrcode.md
+++ docs/components/qrcode.md
@@ -1,6 +1,6 @@
 # Qrcode 二维码
 
-生成二维码
+生成二维码；底层的二维码的渲染是使用 [qrcode-generator-es](https://www.npmjs.com/package/qrcode-generator-es) 进行渲染
 
 ## 引用
 
@@ -14,18 +14,37 @@ regist(Qrcode);
 
 ### 基础用法
 
-使用
+`text` 属性为二维码内容
 
 <ClientOnly>
 <l-code-preview>
 <textarea lang="html">
-  <l-qrcode></l-qrcode>
+  <l-qrcode text="Hello World"></l-qrcode>
 </textarea>
-<div class="source">
+</l-code-preview>
+</ClientOnly>
+
+### 样式调整
+
+可以通过 `fill` 属性更改二维码颜色, `size` 属性更改大小; 注意: 实际的二维码的大小不一定跟传递的 `size` 一样大，因为会调整每一个二维码小单元格尺寸, 所以最终大小只能是小于等于传递的 `size`
+
+<ClientOnly>
+<l-code-preview>
 <textarea lang="html">
-  <l-qrcode></l-qrcode>
+  <l-qrcode text="Hello World" fill="#4998f4" size="120"></l-qrcode>
 </textarea>
-</div>
+</l-code-preview>
+</ClientOnly>
+
+### 图标
+
+可以将 `logo` 放入二维码
+
+<ClientOnly>
+<l-code-preview>
+<textarea lang="html">
+  <l-qrcode text="Hello World" icon="/litos-ui/logo.svg"></l-qrcode>
+</textarea>
 </l-code-preview>
 </ClientOnly>
 
--- src/components/qrcode/index.ts
+++ src/components/qrcode/index.ts
@@ -1,6 +1,8 @@
 import BaseComponent from "../base";
 //@ts-ignore
 import css from "./index.less?inline";
+import { QRCodeRender, renderToCanvas } from "qrcode-generator-es";
+import { $one } from "ph-utils/dom";
 
 export default class Qrcode extends BaseComponent {
   public static baseName = "qrcode";
@@ -13,10 +15,68 @@ export default class Qrcode extends BaseComponent {
   public size?: string | number = "100";
   /** 二维码等级 */
   public level?: "L" | "M" | "Q" | "H" = "M";
+  /** 插入二维码中间的图标 */
+  public icon?: string;
+  private _qrcode?: QRCodeRender;
 
   connectedCallback(): void {
     this.loadStyleText(css);
     super.connectedCallback();
   }
-  render() {}
+
+  static get observedAttributes(): string[] | null | undefined {
+    return ["text", "fill", "size", "level", "icon"];
+  }
+
+  attributeChangedCallback(
+    name: string,
+    oldValue: string,
+    newValue: string
+  ): void {
+    if (oldValue !== newValue) {
+      this[name as "text"] = newValue;
+      this.#updateOption(name, newValue);
+      this.#drawQrcode();
+    }
+  }
+
+  afterInit(): void {
+    this.#drawQrcode();
+  }
+
+  #updateOption(name: string, value: any) {
+    if (this._qrcode) {
+      if (name === "icon") {
+        this._qrcode.option.icon = value ? { src: value } : undefined;
+      } else {
+        this._qrcode.option[name as "text"] = value;
+      }
+    }
+  }
+
+  #drawQrcode() {
+    if (!this._qrcode) {
+      const $el = $one("canvas", this.root);
+      if ($el) {
+        console.log(this.icon);
+        this._qrcode = new QRCodeRender({
+          renderFn: renderToCanvas,
+          text: this.text,
+          size: Number(this.size),
+          level: this.level,
+          fill: this.fill,
+          icon: this.icon ? { src: this.icon } : undefined,
+          el: $el,
+        });
+      }
+    }
+    if (this._qrcode != null && this.text) {
+      console.log(this._qrcode);
+      this._qrcode.render();
+    }
+  }
+
+  render() {
+    return "<canvas class='renderer-el' width='100' height='100'></canvas>";
+  }
 }
